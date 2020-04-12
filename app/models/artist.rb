24b0ef5b48bf4fb1e77695df84760aa8b54cab05
isaacmeedinaa <isaac.meedinaa@gmail.com>
2020-04-12 18:36:07 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,8 @@
 class Artist < ActiveRecord::Base
+    # we are telling the Artist class that it 
+    # has many 'Songs'
+    has_many :songs
+    # we are telling the Artist class that it 
+    # has many 'Genres' through 'Songs'
+    has_many :genres, through: :songs
 end
